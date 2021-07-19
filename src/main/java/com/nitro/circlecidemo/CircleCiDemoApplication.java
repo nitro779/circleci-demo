70c592428959b88af8a31051ad21508962a1344e
Naresh Chakali <chakali.naresh@thoughtworks.com>
2021-07-19 22:52:05 +0530

    Initialize project

---
+++ src/main/java/com/nitro/circlecidemo/CircleCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nitro.circlecidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nitro/circlecidemo/CircleCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitro.circlecidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
